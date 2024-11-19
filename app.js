2b9fad5614a23909964d0f0a59ca9b0bed985341
lekhakachoria <92125444+lekhakachoria@users.noreply.github.com>
2024-11-19 15:28:28 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// fetch("https://api.github.com/users/mulinater")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(` Muli has ${json.followers} followers`);
+//   });
+
+//   console.log(json.id, json.name));
+
+// fetch("https://api.github.com/users/mulinater").then((response) =>
+//   response.json().then((json) => {
+//     json.forEach((project) => {
+//       console.log(project.followers);
+//     });
+//   })
+// );
+
+// Beginning of Task 2
+
+// Question 3
+
+// fetch("https://jsonplaceholder.typicode.com/photos")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     console.log(`Total number of photos is ${json.length}`);
+//   });
+
+// Question 5
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((json) => {
+//     const completedTodos = json.filter((todo) => todo.completed);
+//     console.log(`Total number of completed todos: ${completedTodos.length}`);
+//   })
+//   .catch((error) => console.error("Error fetching todos:", error));
+
+//   question 6
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("Completed Todos:");
+    for (let todo of json) {
+      if (todo.completed) {
+        console.log(`ID: ${todo.id}, Title: ${todo.title}`);
+      }
+    }
+  })
+  .catch((error) => console.error("Error fetching todos:", error));
