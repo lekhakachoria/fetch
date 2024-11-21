4ce79aa7cb2c14d5a7db50d96bc921f8f482e495
Alex <alexmiorth@gmail.com>
2024-11-21 16:32:19 -0600

    added number of public git repos

--- app.js
+++ app.js
@@ -14,6 +14,15 @@
 //   })
 // );
 
+// Task 1 Sample Github API
+
+// Use fetch() to find out the followings:
+// 1. The number of public git repos you have
+fetch("https://api.github.com/users/mulinater")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json.public_repos);
+  });
 // Beginning of Task 2
 
 // Question 3
